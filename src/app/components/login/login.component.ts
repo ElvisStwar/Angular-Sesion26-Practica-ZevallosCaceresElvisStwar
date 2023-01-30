5f5cfb275a3f317b0f18b300a33c9e6130c7fc17
ElvisStwar <elvis.stwar@hotmail.com>
2023-01-30 12:34:57 -0500

    Actualizacion de lista

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup,FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserServiceService } from 'src/app/services/user-service.service';
@@ -8,17 +8,19 @@ import { UserServiceService } from 'src/app/services/user-service.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
 
   formLogin!:FormGroup;
+  datos:any;
+  lista:any;
 
   constructor( 
-    private userService:UserServiceService,
+    private data:UserServiceService,
     private router:Router
     ){
 
     this.formLogin = new FormGroup({
-      email: new FormControl(),
+      username: new FormControl(),
       password: new FormControl()
     })
 
@@ -26,10 +28,31 @@ export class LoginComponent {
   }
 
   ngOnInit(): void {
+
+      this.data.getAll().subscribe(data => console.log(data));
+      this.data.getAll().subscribe(data => this.lista = data);
+  
   }
 
   
   
 
-  onSubmit(){}
+  onSubmit(){
+    this.datos = this.formLogin.value
+    // console.log(this.datos.username)
+    // console.log(this.datos.password)
+    // console.log(this.lista[1])
+
+    for(let i = 0;this.lista[i].username==this.datos.username&&this.lista[i].email==this.datos.password;i++){
+      console.log("logueado")
+      localStorage.setItem('status','logIn')
+    }
+
+    if(localStorage.getItem('status')=="logIn"){
+      this.router.navigate(['lista'])
+    }
+
+
+
+  }
 }
